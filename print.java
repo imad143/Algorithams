04ab55d788bdfb1a43337e0f8399c3139c937961
Imad jahangir <60513884+imad143@users.noreply.github.com>
2022-05-22 17:58:45 +0100

    Create print.java
    how to print statement

---
+++ print.java
@@ -0,0 +1,15 @@
+/**
+ * filename: ILoveCS2004.java
+ * Author: IMAD JAHANGIR
+ * Date: 19 OCTOBER 2020
+ */
+
+public class print {
+
+	public static void main(String[] args) {
+
+		System.out.println("I love CS2004!");
+
+	}
+
+}
